f924bd1a8b38c143b62710af3c22490e6f808a82
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:30:49 -0300

    Filepath: f111/application/shared_components/navigation/lists/page_navigation.sql; Timestamp: 20-MAR-23 02.30.49.424914000 PM +00:00

---
+++ f111/application/shared_components/navigation/lists/page_navigation.sql
@@ -0,0 +1,37 @@
+prompt --application/shared_components/navigation/lists/page_navigation
+begin
+--   Manifest
+--     LIST: Page Navigation
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_list(
+ p_id=>wwv_flow_imp.id(18556575407106098)
+,p_name=>'Page Navigation'
+,p_list_status=>'PUBLIC'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(18556931900106099)
+,p_list_item_display_sequence=>20
+,p_list_item_link_text=>'Barcode Search'
+,p_list_item_link_target=>'f?p=&APP_ID.:2:&APP_SESSION.::&DEBUG.:::'
+,p_list_item_icon=>'fa-table-search'
+,p_list_item_current_type=>'TARGET_PAGE'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(18557375796106099)
+,p_list_item_display_sequence=>30
+,p_list_item_link_text=>'Barcode Report'
+,p_list_item_link_target=>'f?p=&APP_ID.:3:&APP_SESSION.::&DEBUG.:::'
+,p_list_item_icon=>'fa-table'
+,p_list_item_current_type=>'TARGET_PAGE'
+);
+wwv_flow_imp.component_end;
+end;
+/
