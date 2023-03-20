6d5b76533f2706589cb16952f8aee6e246f0b863
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:32:25 -0300

    Filepath: f111/application/pages/page_10026.sql; Timestamp: 20-MAR-23 02.32.25.072129000 PM +00:00

---
+++ f111/application/pages/page_10026.sql
@@ -0,0 +1,196 @@
+prompt --application/pages/page_10026
+begin
+--   Manifest
+--     PAGE: 10026
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_page.create_page(
+ p_id=>10026
+,p_name=>'Log Messages'
+,p_alias=>'LOG-MESSAGES'
+,p_page_mode=>'MODAL'
+,p_step_title=>'Log Messages'
+,p_warn_on_unsaved_changes=>'N'
+,p_autocomplete_on_off=>'OFF'
+,p_group_id=>wwv_flow_imp.id(18526628665105421)
+,p_page_template_options=>'#DEFAULT#'
+,p_required_role=>wwv_flow_imp.id(18525349433105414)
+,p_required_patch=>wwv_flow_imp.id(18523391763105410)
+,p_dialog_chained=>'N'
+,p_protection_level=>'C'
+,p_page_component_map=>'03'
+,p_last_updated_by=>'JURKLEIN'
+,p_last_upd_yyyymmddhh24miss=>'20230201114704'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(18638495425106270)
+,p_plug_name=>'Automation Execution'
+,p_region_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'Y'
+,p_plug_template=>wwv_flow_imp.id(18357385730105218)
+,p_plug_display_sequence=>10
+,p_plug_query_num_rows=>15
+,p_attribute_01=>'N'
+,p_attribute_02=>'HTML'
+,p_attribute_03=>'Y'
+);
+wwv_flow_imp_page.create_report_region(
+ p_id=>wwv_flow_imp.id(18640869818106271)
+,p_name=>'Messages'
+,p_template=>wwv_flow_imp.id(18382331288105232)
+,p_display_sequence=>10
+,p_region_template_options=>'#DEFAULT#:t-IRR-region--noBorders'
+,p_component_template_options=>'#DEFAULT#:t-Report--stretch:t-Report--altRowsDefault:t-Report--rowHighlight:t-Report--inline'
+,p_source_type=>'NATIVE_SQL_REPORT'
+,p_query_type=>'SQL'
+,p_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select message_timestamp,',
+'       message,',
+'       message_type,',
+'       pk_value',
+'  from apex_automation_msg_log',
+' where automation_log_id = :P10026_LOG_ID'))
+,p_ajax_enabled=>'Y'
+,p_lazy_loading=>false
+,p_query_row_template=>wwv_flow_imp.id(18458851862105275)
+,p_query_num_rows=>50
+,p_query_options=>'DERIVED_REPORT_COLUMNS'
+,p_query_no_data_found=>'no data found'
+,p_query_num_rows_type=>'NEXT_PREVIOUS_LINKS'
+,p_query_row_count_max=>50
+,p_pagination_display_position=>'BOTTOM_RIGHT'
+,p_prn_output=>'N'
+,p_prn_format=>'PDF'
+,p_sort_null=>'L'
+,p_plug_query_strip_html=>'N'
+);
+wwv_flow_imp_page.create_report_columns(
+ p_id=>wwv_flow_imp.id(18641200617106275)
+,p_query_column_id=>1
+,p_column_alias=>'MESSAGE_TIMESTAMP'
+,p_column_display_sequence=>1
+,p_column_heading=>'Timestamp'
+,p_use_as_row_header=>'N'
+,p_column_alignment=>'CENTER'
+,p_default_sort_column_sequence=>1
+,p_default_sort_dir=>'desc'
+,p_disable_sort_column=>'N'
+,p_include_in_export=>'Y'
+);
+wwv_flow_imp_page.create_report_columns(
+ p_id=>wwv_flow_imp.id(18641656671106275)
+,p_query_column_id=>2
+,p_column_alias=>'MESSAGE'
+,p_column_display_sequence=>2
+,p_column_heading=>'Message'
+,p_use_as_row_header=>'N'
+,p_heading_alignment=>'LEFT'
+,p_disable_sort_column=>'N'
+,p_include_in_export=>'Y'
+);
+wwv_flow_imp_page.create_report_columns(
+ p_id=>wwv_flow_imp.id(18642051154106275)
+,p_query_column_id=>3
+,p_column_alias=>'MESSAGE_TYPE'
+,p_column_display_sequence=>3
+,p_column_heading=>'Message Type'
+,p_use_as_row_header=>'N'
+,p_heading_alignment=>'LEFT'
+,p_disable_sort_column=>'N'
+,p_include_in_export=>'Y'
+);
+wwv_flow_imp_page.create_report_columns(
+ p_id=>wwv_flow_imp.id(18642420898106276)
+,p_query_column_id=>4
+,p_column_alias=>'PK_VALUE'
+,p_column_display_sequence=>4
+,p_column_heading=>'Primary Key Value'
+,p_use_as_row_header=>'N'
+,p_heading_alignment=>'LEFT'
+,p_disable_sort_column=>'N'
+,p_include_in_export=>'Y'
+);
+wwv_flow_imp_page.create_page_item(
+ p_id=>wwv_flow_imp.id(18638877163106270)
+,p_name=>'P10026_LOG_ID'
+,p_item_sequence=>10
+,p_item_plug_id=>wwv_flow_imp.id(18638495425106270)
+,p_use_cache_before_default=>'NO'
+,p_display_as=>'NATIVE_HIDDEN'
+,p_lov_display_extra=>'NO'
+,p_protection_level=>'S'
+,p_attribute_01=>'Y'
+);
+wwv_flow_imp_page.create_page_item(
+ p_id=>wwv_flow_imp.id(18639223128106270)
+,p_name=>'P10026_AUTOMATION_NAME'
+,p_item_sequence=>20
+,p_item_plug_id=>wwv_flow_imp.id(18638495425106270)
+,p_prompt=>'Automation'
+,p_display_as=>'NATIVE_DISPLAY_ONLY'
+,p_begin_on_new_line=>'N'
+,p_field_template=>wwv_flow_imp.id(18493741275105303)
+,p_lov_display_extra=>'NO'
+,p_attribute_01=>'Y'
+,p_attribute_02=>'VALUE'
+,p_attribute_04=>'Y'
+,p_attribute_05=>'PLAIN'
+);
+wwv_flow_imp_page.create_page_item(
+ p_id=>wwv_flow_imp.id(18639657817106271)
+,p_name=>'P10026_START_TIMESTAMP'
+,p_item_sequence=>30
+,p_item_plug_id=>wwv_flow_imp.id(18638495425106270)
+,p_prompt=>'Started'
+,p_display_as=>'NATIVE_DISPLAY_ONLY'
+,p_begin_on_new_line=>'N'
+,p_field_template=>wwv_flow_imp.id(18493741275105303)
+,p_lov_display_extra=>'NO'
+,p_attribute_01=>'Y'
+,p_attribute_02=>'VALUE'
+,p_attribute_04=>'Y'
+,p_attribute_05=>'PLAIN'
+);
+wwv_flow_imp_page.create_page_item(
+ p_id=>wwv_flow_imp.id(18640067189106271)
+,p_name=>'P10026_STATUS'
+,p_item_sequence=>40
+,p_item_plug_id=>wwv_flow_imp.id(18638495425106270)
+,p_prompt=>'Status'
+,p_display_as=>'NATIVE_DISPLAY_ONLY'
+,p_begin_on_new_line=>'N'
+,p_field_template=>wwv_flow_imp.id(18493741275105303)
+,p_lov_display_extra=>'NO'
+,p_attribute_01=>'Y'
+,p_attribute_02=>'VALUE'
+,p_attribute_04=>'Y'
+,p_attribute_05=>'PLAIN'
+);
+wwv_flow_imp_page.create_page_process(
+ p_id=>wwv_flow_imp.id(18640475529106271)
+,p_process_sequence=>10
+,p_process_point=>'BEFORE_HEADER'
+,p_process_type=>'NATIVE_PLSQL'
+,p_process_name=>'Get Log Detail'
+,p_process_sql_clob=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select automation_name,',
+'       start_timestamp,',
+'       status',
+'  into :P10026_AUTOMATION_NAME,',
+'       :P10026_START_TIMESTAMP,',
+'       :P10026_STATUS',
+'  from apex_automation_log',
+' where id = :P10026_LOG_ID;'))
+,p_process_clob_language=>'PLSQL'
+,p_error_display_location=>'INLINE_IN_NOTIFICATION'
+);
+wwv_flow_imp.component_end;
+end;
+/
