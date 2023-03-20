58b4ddb52f6954b7f3d3b2438068ce30b7ae0265
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:31:09 -0300

    Filepath: f111/application/shared_components/security/app_access_control/administrator.sql; Timestamp: 20-MAR-23 02.31.08.883921000 PM +00:00

---
+++ f111/application/shared_components/security/app_access_control/administrator.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/security/app_access_control/administrator
+begin
+--   Manifest
+--     ACL ROLE: Administrator
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_acl_role(
+ p_id=>wwv_flow_imp.id(18524955562105413)
+,p_static_id=>'ADMINISTRATOR'
+,p_name=>'Administrator'
+,p_description=>'Role assigned to application administrators.'
+);
+wwv_flow_imp.component_end;
+end;
+/
