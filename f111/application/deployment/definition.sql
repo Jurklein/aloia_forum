10c34db524c62699b5e40daea4238162970fad8c
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:32:29 -0300

    Filepath: f111/application/deployment/definition.sql; Timestamp: 20-MAR-23 02.32.29.010109000 PM +00:00

---
+++ f111/application/deployment/definition.sql
@@ -0,0 +1,21 @@
+prompt --application/deployment/definition
+begin
+--   Manifest
+--     INSTALL: 111
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_install(
+ p_id=>wwv_flow_imp.id(32413038148876047)
+,p_required_free_kb=>100
+,p_required_sys_privs=>'CREATE PROCEDURE:CREATE TABLE:CREATE TRIGGER:CREATE VIEW'
+);
+wwv_flow_imp.component_end;
+end;
+/
