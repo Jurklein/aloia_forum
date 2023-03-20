a9ce5df12a3ead7681bd8f1a8a5dbbd6f7660845
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:31:15 -0300

    Filepath: f111/application/shared_components/user_interface/lovs/email_username_format.sql; Timestamp: 20-MAR-23 02.31.14.698139000 PM +00:00

---
+++ f111/application/shared_components/user_interface/lovs/email_username_format.sql
@@ -0,0 +1,28 @@
+prompt --application/shared_components/user_interface/lovs/email_username_format
+begin
+--   Manifest
+--     EMAIL_USERNAME_FORMAT
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_list_of_values(
+ p_id=>wwv_flow_imp.id(18667518169106307)
+,p_lov_name=>'EMAIL_USERNAME_FORMAT'
+,p_lov_query=>'.'||wwv_flow_imp.id(18667518169106307)||'.'
+,p_location=>'STATIC'
+);
+wwv_flow_imp_shared.create_static_lov_data(
+ p_id=>wwv_flow_imp.id(18667821874106307)
+,p_lov_disp_sequence=>1
+,p_lov_disp_value=>'Email Addresses'
+,p_lov_return_value=>'EMAIL'
+);
+wwv_flow_imp.component_end;
+end;
+/
