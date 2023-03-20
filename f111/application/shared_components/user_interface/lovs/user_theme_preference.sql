52e96b6457cb2b71d7aa3f82e9a34a70851fedf6
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:31:16 -0300

    Filepath: f111/application/shared_components/user_interface/lovs/user_theme_preference.sql; Timestamp: 20-MAR-23 02.31.15.958374000 PM +00:00

---
+++ f111/application/shared_components/user_interface/lovs/user_theme_preference.sql
@@ -0,0 +1,28 @@
+prompt --application/shared_components/user_interface/lovs/user_theme_preference
+begin
+--   Manifest
+--     USER_THEME_PREFERENCE
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_list_of_values(
+ p_id=>wwv_flow_imp.id(18562004794106114)
+,p_lov_name=>'USER_THEME_PREFERENCE'
+,p_lov_query=>'.'||wwv_flow_imp.id(18562004794106114)||'.'
+,p_location=>'STATIC'
+);
+wwv_flow_imp_shared.create_static_lov_data(
+ p_id=>wwv_flow_imp.id(18562325416106115)
+,p_lov_disp_sequence=>1
+,p_lov_disp_value=>'Allow End Users to choose Theme Style'
+,p_lov_return_value=>'Yes'
+);
+wwv_flow_imp.component_end;
+end;
+/
