5538be7a5757f65b4b79a9f36a3c6543ed28a481
Jurklein <lucasklein01@gmail.com>
2023-03-20 11:31:10 -0300

    Filepath: f111/application/shared_components/security/app_access_control/reader.sql; Timestamp: 20-MAR-23 02.31.10.182910000 PM +00:00

---
+++ f111/application/shared_components/security/app_access_control/reader.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/security/app_access_control/reader
+begin
+--   Manifest
+--     ACL ROLE: Reader
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.10.07'
+,p_release=>'22.2.4'
+,p_default_workspace_id=>7870050665145974
+,p_default_application_id=>111
+,p_default_id_offset=>0
+,p_default_owner=>'JURKLEIN'
+);
+wwv_flow_imp_shared.create_acl_role(
+ p_id=>wwv_flow_imp.id(18525222974105414)
+,p_static_id=>'READER'
+,p_name=>'Reader'
+,p_description=>'Role assigned to application readers.'
+);
+wwv_flow_imp.component_end;
+end;
+/
